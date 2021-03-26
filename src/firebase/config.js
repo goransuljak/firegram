80243e13fa661ecb5414309f645b25b504df1b3c
Goran <goranfrance@hotmail.com>
2021-03-26 21:19:08 +0000

    dfa

---
+++ src/firebase/config.js
@@ -0,0 +1,11 @@
+ // Your web app's Firebase configuration
+ var firebaseConfig = {
+    apiKey: "AIzaSyDw7s0bs8eTOSiQjjtFsHgwxljnLzSqQC0",
+    authDomain: "ninja-firegram-cf87a.firebaseapp.com",
+    projectId: "ninja-firegram-cf87a",
+    storageBucket: "ninja-firegram-cf87a.appspot.com",
+    messagingSenderId: "678776123411",
+    appId: "1:678776123411:web:54418a3d69f9b66afd4fe6"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
